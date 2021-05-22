ced73be40e8af9be44d42a88886d5e1ea2aed789
jay <jay172bhat@gmail.com>
2021-05-22 10:58:48 +0530

    Second commit

--- src/Components/Promotions.js
+++ src/Components/Promotions.js
@@ -8,6 +8,7 @@ import Himanshu from '../Images/Promotions/Himanshu.png';
 import Piyush from '../Images/Promotions/Piyush.png';
 import Pushpankar from '../Images/Promotions/Pushpankar.png';
 import Tushar from '../Images/Promotions/Tushar.png';
+import noImg from '../Images/BSTC/Chetan.png';
 
 function Promotions(){
     return(
@@ -15,48 +16,124 @@ function Promotions(){
             <Container>
             <h1 className='promotion-head' >Promotions</h1>
             <p className='promotion-intro'>Celebrating our employee’s achievements as they are Encouraging others to strive for advancement</p>
-                <Row className='bstc-winner'>
+                <div className='designation'>
+                 <Row>
                     <Col>
-                        <img src={Harpaal} className='winner-img' alt='Harpaal' />
-                        <h3 className='bstc-name' >Harpaal Cheema</h3>
-                        <h6>Associate Project Manager</h6>
+                        <h4 className='designation-head'>Senior Manager</h4>
                     </Col>
-                    
+                 </Row>
+                 <Row>
                     <Col>
-                        <img src={Chetna} className='winner-img' alt='Chetna' />
-                        <h3 className='bstc-name' >Chetna Deshwal</h3>
-                        <h6>QA Test Lead</h6>
+                    <img src={noImg} className='winner-img' alt='Deep' />
+                        <h3 className='bstc-name' >Deep Singh Rajput</h3>
                     </Col>
+                 </Row>
+                </div>
+                <hr />
+                <div className='designation'>
+                    <Row>
+                       <Col>
+                          <h4 className='designation-head'>Senior Executive Talent Acquisition</h4>
+                       </Col>
+                     </Row>
+                     <Row>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Vandana' />
+                          <h3 className='bstc-name' >Vandna Verma Kataria</h3>
+                       </Col>
+                     </Row>
+                </div>
+                <hr />
+                <div className='designation'>
+                    <Row>
+                       <Col>
+                           <h4 className='designation-head'>Associate Project Manager</h4>
+                       </Col>
+                    </Row>
+                    <Row>
+                       <Col>
+                          <img src={Harpaal} className='winner-img' alt='Harpaal' />
+                          <h3 className='bstc-name' >Harpal Singh Cheema</h3>
+                       </Col>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Thomas' />
+                          <h3 className='bstc-name' >Thomas George</h3>
+                       </Col>
+                    </Row>
+                    <Row>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Shashwat' />
+                          <h3 className='bstc-name' >Shashwat Iyer</h3>
+                       </Col>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Indrani' />
+                          <h3 className='bstc-name' >Indrani Chowdhury</h3>
+                       </Col>
+                    </Row>
+                </div>
+                <hr />
+                <div className='designation'>
+                    <Row>
                     <Col>
-                        <img src={Arman} className='winner-img' alt='Arman' />
-                        <h3 className='bstc-name' >Arman Tiwari</h3>
-                        <h6>Senior Tester</h6>
-                    </Col>
-                </Row>
-                <Row className='bstc-winner'>
-                    <Col>
-                        <img src={Himanshu} className='winner-img' alt='Himanshu' />
-                        <h3 className='bstc-name' >Himanshu Parashar</h3>
-                        <h6>Senior Tester</h6>
-                    </Col>
-                    <Col>
-                        <img src={Piyush} className='winner-img' alt='Piyush' />
-                        <h3 className='bstc-name' >Piyush Bhadani</h3>
-                        <h6>Senior Tester</h6>
-                    </Col>
-                    <Col>
-                        <img src={Tushar} className='winner-img' alt='Tushar' />
-                        <h3 className='bstc-name' >Tushar Tikshanshu</h3>
-                        <h6>Senior Tester</h6>
-                    </Col>
-                </Row>
-                <Row className='bstc-winner'>
-                <Col>
-                        <img src={Pushpankar} className='winner-img' alt='Pushpankar' />
-                        <h3 className='bstc-name' >Pushpankar Kiran</h3>
-                        <h6>Senior Tester</h6>
-                    </Col>
-                </Row>
+                           <h4 className='designation-head'>QA Test Lead</h4>
+                       </Col>
+                    </Row>
+                    <Row>
+                       <Col>
+                          <img src={Chetna} className='winner-img' alt='Chetna' />
+                          <h3 className='bstc-name' >Chetna Deshwal</h3>
+                       </Col>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Ashish' />
+                          <h3 className='bstc-name' >Ashish Kumar</h3>
+                       </Col>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Aman' />
+                          <h3 className='bstc-name' >Aman Bedi</h3>
+                       </Col>
+                    </Row>
+                </div>
+                <hr />
+                <div className='designation'>
+                    <Row>
+                       <Col>
+                           <h4 className='designation-head'>Senior Tester</h4>
+                       </Col>
+                    </Row>
+                    <Row>
+                       <Col>
+                          <img src={Arman} className='winner-img' alt='Arman' />
+                          <h3 className='bstc-name' >Arman Tiwari</h3>
+                          
+                       </Col>
+                       <Col>
+                           <img src={Himanshu} className='winner-img' alt='Himanshu' />
+                           <h3 className='bstc-name' >Himanshu Parashar</h3>
+                           
+                       </Col>
+                       <Col>
+                           <img src={Piyush} className='winner-img' alt='Piyush' />
+                           <h3 className='bstc-name' >Piyush Bhadani</h3>
+                           
+                      </Col>
+                    </Row>
+                    <Row>
+                       <Col>
+                           <img src={Tushar} className='winner-img' alt='Tushar' />
+                           <h3 className='bstc-name' >Tushar Tikshanshu</h3>
+                           
+                       </Col>
+                       <Col>
+                           <img src={Pushpankar} className='winner-img' alt='Pushpankar' />
+                           <h3 className='bstc-name' >Pushpankar Kiran</h3>
+                           
+                      </Col>
+                       <Col>
+                          <img src={noImg} className='winner-img' alt='Rahul' />
+                          <h3 className='bstc-name' >Rahul mathur</h3>
+                       </Col>
+                    </Row>
+                </div>
             </Container>
         </div>
     )
