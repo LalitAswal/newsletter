03ad5e9eabf2178cc91182b303e2811b75e6f1de
jay <jay172bhat@gmail.com>
2021-05-30 19:25:01 +0530

    full n final commit

--- src/Components/Date.js
+++ src/Components/Date.js
@@ -4,7 +4,7 @@ import './Styles/Heading.css';
 function Date(){
     return(
         <div className='date'>
-            <strong className='babel-date'>Monday, 24.05.2021</strong>
+            <strong className='babel-date'>Monday, 31.05.2021</strong>
         </div>
     )
 }
