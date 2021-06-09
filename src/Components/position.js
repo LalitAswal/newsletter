7c40196142f6c0d249970eebc5576411247a0a91
JayB172 <63243399+JayB172@users.noreply.github.com>
2021-06-09 09:00:48 +0530

    Update position.js

--- src/Components/position.js
+++ src/Components/position.js
@@ -5,12 +5,8 @@ function position(){
         <div className='openings'>
             <h4>We are looking for a web developer to join our team</h4>
             <h5 className='skill'>Skills required: Basic knowledge of HTML, CSS and Javascript</h5>
-            <h5 className='skill'>Contact persons</h5>
-            <h6 className='contactp'>Karan Sharma (karsharma@keywordsstudios.com)</h6>
-            <h6 className='contactp'>Vimmy Vashisht (vvashisht@keywordsstudios.com)</h6>
-            <h6 className='contactp'>Yash Bhatnagar (ybhatnagar@keywordsstudios.com)</h6>
-            <h6 className='contactp'>Rahat Pal(rpal@keywordsstudios.com)</h6>
-            <h6 className='contactp'>Jay Bhat (JBhat@keywordsstudios.com)</h6>
+            <h5 className='skill'>Reach Us at</h5>
+            <h6 className='contactp'>BabelNews@keywordsstudios.com</h6>
         </div>
     )
 }
